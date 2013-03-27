4a988ed9a6b03f6326225e3ed2e0b14a3b250fe6
Marc Risney <marc.risney@gmail.com>
2013-03-26 18:44:25 -0700

    removed non necessary class, added comments

--- src/org/risney/adf/utils/ImageUtils.java
+++
@@ -1,8 +0,0 @@
-package org.risney.adf.utils;
-
-public class ImageUtils {
-
-    public ImageUtils() {
-        super();
-    }
-}
--- src/org/risney/adf/view/LeafletManagedBean.java
+++ src/org/risney/adf/view/LeafletManagedBean.java
@@ -33,14 +33,15 @@ public class LeafletManagedBean {
     /**
      *
      * @param searchString
-     * @return
+     * @return List<javax.faces.model.SelectItem>
+     *
+     * An array is created from candiates, derived  from the
+     * the dataControl method 'fuzzySearch' passing, in whatever searchString is keyed
+     * into the RichInputText locationInputText. 
+     * Not much to see here, this is standard AutoComplete JSF pattern.
+     * 
      */
     public List getSuggestedLocations(String searchString) {
-
-        // An array is created from candiates, derived  from the
-        // the dataControl method 'fuzzySearch' passing, in whatever searchString is keyed
-        // into the RichInputText locationInputText;
-
         List<SelectItem> suggestedLocations = new ArrayList<SelectItem>();
         BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
         OperationBinding operationBinding = bindings.getOperationBinding("fuzzySearch");
@@ -58,13 +59,16 @@ public class LeafletManagedBean {
         return suggestedLocations;
     }
 
-
+    /**
+     *
+     * @param valueChangeEvent
+     * Capture the auto-completed value,
+     * executing the DataControl method 'getLocationAndWeatherWithPlaceName'
+     * getting back a Location object.
+     * Then calling the method 'zoomToLocation' in adf-leaflet.js
+     *
+     */
     public void onLocationSearchValueChange(ValueChangeEvent valueChangeEvent) {
-
-        // Capture the auto-completed value,
-        // executing the DataControl method 'getLocationAndWeatherWithPlaceName'
-        // getting back a Location object, executing the
-
         if (valueChangeEvent.getNewValue() != null) {
             String placeName = valueChangeEvent.getNewValue().toString();
             BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
@@ -79,6 +83,15 @@ public class LeafletManagedBean {
     /**
      *
      * @param clientEvent
+     *
+     * This is essentially a proxy method, that the AJAX/XMLHTTPRequest passes information into
+     * in ad-leaflet.js, the method onMapClick passes in the coordinates as a Map of Double
+     * coordinates. Then the data control method is called 'getLocationAndWeatherWithCoordinates'
+     * returning the geonames.org webservice data (the location/ the weather). Then a JSON object
+     * is passed back to the  ad-leaflet.js method 'zoomToLocation'
+     * To some degree this could be considered unnecessary newtork hops. But this is more for demonstration
+     * purposes. This pattern could be used for integrating enterprise data with a .js framwework.
+     * 
      */
 
     public void serverEventHandler(ClientEvent clientEvent) {
@@ -112,7 +125,12 @@ public class LeafletManagedBean {
      *
      * @param script
      *
+     * This generic helper method, allows arbitray javascript to be executed from the
+     * managed bean, instead of a specific Rich Faces Partial Page Rendering, the
+     * ExtendedRenderKitService allows methods to call any third party JS methods.
+     * Extremely useful method.
      * example: executeJavaScript("alert('hello world')");
+     * 
      */
     public void executeJavaScript(final String script) {
         ExtendedRenderKitService service = (ExtendedRenderKitService)Service.getRenderKitService(FacesContext.getCurrentInstance(), ExtendedRenderKitService.class);
